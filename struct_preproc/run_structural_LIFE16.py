44813c65a260f6d483e18a2c5b2ac91df7601e5b
fliem <franz.liem>
2016-01-21 20:18:24 +0100

    LIFE16

--- struct_preproc/run_structural_LIFE16.py
+++ struct_preproc/run_structural_LIFE16.py
@@ -4,7 +4,7 @@ Created on Mon Feb  9 11:10:25 2015
 
 @author: fbeyer
 run:
-python struct_preproc/run_structural_sample5.py f /scr/kennedy2/liem/subjects_lists/subjects_big_sample5_2_clean_n1184.txt
+python struct_preproc/run_structural_LIFE16.py f /scr/adenauer2/Franz/LIFE16/LIFE16_subjects_list_n2559.txt
 
 """
 
@@ -27,19 +27,18 @@ elif mode == 'f':
     with open(sys.argv[2], 'r') as f:
         subjects = [line.strip() for line in f]
 
+# select fold
+subjects = subjects[:10]
+
 for subject in subjects:
     print 'Running subject ' + subject
-    root_dir = '/scr/kennedy2/liem/sample_5'
+    root_dir = '/nobackup/clustercache/liem/LIFE'
     working_dir = os.path.join(root_dir, 'wd', subject)
-    data_dir = os.path.join(root_dir, 'subjects', subject)
-    out_dir = data_dir
+    data_dir = os.path.join(root_dir, subject)
+    out_dir = os.path.join(root_dir, subject, 'preprocessed', 'structural')
 
-    ###########careful
-    freesurfer_dir = '/scr/kennedy2/LIFE//freesurfer_all/'
-    # # #
+    freesurfer_dir = '/scr/kennedy2/LIFE/freesurfer_all/'
 
-
-    ##########you have to change this!!
     standard_brain = '/usr/share/fsl/5.0/data/standard/MNI152_T1_2mm_brain.nii.gz'
     create_structural(subject=subject, working_dir=working_dir, data_dir=data_dir,
                       freesurfer_dir=freesurfer_dir, out_dir=out_dir,
