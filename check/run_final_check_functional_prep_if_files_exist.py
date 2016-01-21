3d9b5192f45a1dec496c151df81d5da20e1fc1b8
fliem <franz.liem>
2016-01-21 19:58:43 +0100

    checks

--- check/run_final_check_functional_prep_if_files_exist.py
+++ check/run_final_check_functional_prep_if_files_exist.py
@@ -1,4 +1,3 @@
-
 from utils import check_if_wf_is_ok
 import os, glob
 
@@ -6,30 +5,37 @@ import os, glob
 
 
 
-#wd_path = '/scr/kansas1/liem/sample_5/wd'
-wd_path = '/nobackup/clustercache/liem/sample_5/wd'
-wf = 'lemon_resting'
-batch_path_template = os.path.join(wd_path, '{subject_id}', wf, 'batch')
-crash_path_template = os.path.join(wd_path, '{subject_id}', 'crash_files')
-check_file_list = ['/scr/kennedy2/liem/sample_5/subjects/{subject_id}/preprocessed/mod/resting/ants/rest_mni_unsmoothed_fullspectrum.nii.gz']
+# wd_path = '/scr/kansas1/liem/sample_5/wd'
+# wd_path = '/nobackup/clustercache/liem/sample_5/wd'
+# wf = 'lemon_resting'
 
-os.chdir(wd_path)
-subjects_list = glob.glob('LI*')
+root_dir = '/scr/kennedy2/liem/sample_5/subjects/'
 
+check_file_list = ['{subject_id}/preprocessed/mod/resting/ants/rest_mni_unsmoothed_fullspectrum.nii.gz',
+                   '{subject_id}/preprocessed/mod/resting/ants/rest_mni_unsmoothed.nii.gz',
+                   '{subject_id}/preprocessed/mod/resting/denoise/rest_preprocessed_nativespace_fullspectrum.nii.gz',
+                   '{subject_id}/preprocessed/mod/resting/denoise/rest_preprocessed_nativespace.nii.gz',
+                   '{subject_id}/preprocessed/mod/resting/FWHM6/rest_mni_smoothed.nii',
+                   '{subject_id}/preprocessed/mod/resting/realign/rest_realigned.par',
+                   '{subject_id}/preprocessed/mod/anat/transforms2mni/transform0GenericAffine.mat',
+                   '{subject_id}/preprocessed/mod/anat/transforms2mni/transform1InverseWarp.nii.gz',
+                   '{subject_id}/preprocessed/mod/anat/transforms2mni/transform1Warp.nii.gz',
+                   ]
 
-everything_ok = check_if_wf_is_ok(batch_path_template, crash_path_template, subjects_list)
+with open('/home/raid2/liem/Dropbox/LeiCa/LIFE/subjects_lists/subjects_big_sample5_2_preprocessing_ok_n1181.txt', 'r') as f:
+    subjects_list = [line.strip() for line in f]
 
-#
-# print('\n\n CHECKING FOR MISSING FILES...')
-# missing_files = False
-# for subject_id in subjects_list:
-#     for file_template in check_file_list:
-#         subject_file = file_template.format(subject_id=subject_id)
-#         if not os.path.exists(subject_file):
-#             print('%s MISSING FILE'%subject_id)
-#             missing_files = True
-#
-# if missing_files:
-#     print('\n SOME FILES MISSING')
-# else:
-#     print('\n ALL OK: files are there')
+print('\n\n CHECKING FOR MISSING FILES...')
+missing_files = False
+for subject_id in subjects_list:
+    for file_template in check_file_list:
+        subject_file = os.path.join(root_dir, file_template.format(subject_id=subject_id))
+
+        if not os.path.exists(subject_file):
+            print('MISSING FILE %s: %s' % (subject_id, subject_file))
+            missing_files = True
+
+if missing_files:
+    print('\n SOME FILES MISSING')
+else:
+    print('\n ALL OK: files are there (%s subjects)'%len(subjects_list))
