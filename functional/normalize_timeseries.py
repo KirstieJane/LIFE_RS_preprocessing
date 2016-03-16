d198839f3c6cf1fbd145a19328d2cb2231c702e3
fliem <franz.liem>
2016-03-16 07:59:38 +0100

    fix header

--- functional/normalize_timeseries.py
+++ functional/normalize_timeseries.py
@@ -17,7 +17,8 @@ def time_normalizer(in_file, tr):
     T = io.time_series_from_file(in_file, normalize=None,
                                  TR=tr)  # previously: normalize="zscore" (percent) as Allen states that variance normalisation degrades inter-subject amplitude and shapes of the components
     normalized_data = T.data
-    out_img = nib.Nifti1Image(normalized_data, nib.load(in_file).get_affine())
+    img = nib.load(in_file)
+    out_img = nib.Nifti1Image(normalized_data, img.get_affine(), header=img.header)
     out_file = fname_presuffix(in_file, suffix='_norm', newpath=os.getcwd())
     out_img.to_filename(out_file)
     return out_file
